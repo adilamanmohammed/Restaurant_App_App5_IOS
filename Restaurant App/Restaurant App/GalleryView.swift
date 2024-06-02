c8d82607e61e91eae01a19121e769d974ac49885
Adil Aman Mohammed <adil.mohammed@okstate.edu>
2024-06-02 16:00:14 -0400

    Gallery View Created

--- Restaurant App/Restaurant App/GalleryView.swift
+++ Restaurant App/Restaurant App/GalleryView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State var photoData = [String]()
+    var DataServices = DataService()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading){
+            
+            Text("Gallery").font(.largeTitle)
+                .bold()
+            
+            GeometryReader{ proxy in
+                
+                ScrollView(showsIndicators : false){
+                    
+                    LazyVGrid(columns: [GridItem(spacing: 10),
+                                        GridItem(spacing: 10),
+                                        GridItem(spacing: 10)
+                                       ]){
+                        
+                        ForEach(photoData, id: \.self){ p in
+                            
+                            Image(p).resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: (proxy.size.width-20)/3)
+                                .clipped()
+//                                .cornerRadius(15.0)
+                            
+                            
+                        }
+                        
+                        
+                    }
+                }
+            }
+            
+            
+        }
+        .padding(.horizontal)
+        .onAppear(){
+            photoData = DataServices.getPhotos()
+        }
+        
+        
     }
+    
 }
 
 #Preview {
